098b89616c495f3c803b7461e5002cb081d5fc72
Iman Heidari <iman.heidari@gmail.com>
2018-07-22 23:44:07 -0700

    Add gh-pages for deploy and also add router and menu for home

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-import AccordionDemo from './components/Accordion/AccordionDemo';
+import AccordionDemo from "./components/Accordion/AccordionDemo";
 
-import './App.css';
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <AccordionDemo />        
-      </div>
+      <Router>
+        <div className="App">
+          <ul>
+            <li>
+              <Link to="/accordion">Accordion</Link>
+            </li>
+          </ul>
+
+          <hr />
+
+          <Route path="/accordion" component={AccordionDemo} />
+        </div>
+      </Router>
     );
   }
 }
